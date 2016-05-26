acd4b53fa3c441d965d3660705a8ba5bec98eba4
suvonkar <suvonkar.dam@scanstock.in>
2016-05-26 17:11:51 +0530

    rallygridboard is added for adding the feature export to csv

--- App.js
+++ App.js
@@ -156,10 +156,11 @@ Ext.define('CustomApp', {
 
 		var that = this;
 
-		Ext.create('Rally.data.wsapi.Store', {
-			model : 'User Story',
+		Ext.create('Rally.data.wsapi.TreeStoreBuilder').build({
+			model : [ 'userstory' ],
 			autoLoad : true,
 			compact : false,
+			enableHierarchy : true,
 			context : {
 				workspace : Rally.environment.getContext().getWorkspace()._ref,
 				project : Rally.environment.getContext().getProject()._ref,
@@ -169,17 +170,14 @@ Ext.define('CustomApp', {
 			filters : that._getGridFilter(),
 			fetch : [ 'FormattedID', 'Name', 'Feature', 'Project', 'ScheduleState' ],
 			limit : Infinity,
-			listeners : {
-				load : function(store, data, success) {
-					//console.log(data);
-					that._drawGrid(store);
-				},
-				scope : this
-			},
 			sorters : [ {
 				property : 'Feature',
 				direction : 'ASC'
 			} ]
+		}).then({
+			success : function(store) {
+				that._drawGrid(store);
+			}
 		});
 
 	},
@@ -233,101 +231,55 @@ Ext.define('CustomApp', {
 	_drawGrid : function(gridStore) {
 		var that = this;
 
-		if (that.down('#ExportBtn')) {
-			that.down('#ExportBtn').destroy();
-		}
-
-		var exportBtn = Ext.create('Ext.Button', {
-			id : 'ExportBtn',
-			text : 'Export',
-			scale : 'large',
-			cls : 'custExprtBtnCls',
-			handler : function() {
-				//alert('Export');
-				that._onClickExport();
-			}
-		});
-
-		this.add(exportBtn);
-
-		if (that.down('rallygrid')) {
-			that.down('rallygrid').destroy();
+		if (that.down('rallygridboard')) {
+			that.down('rallygridboard').destroy();
 		}
 
 		this.add({
-			xtype : 'rallygrid',
+			xtype : 'rallygridboard',
 			id : 'gridData',
-			columnCfgs : [ {
-				text : 'User Story#',
-				dataIndex : 'FormattedID'
-			}, {
-				text : 'User Story Name',
-				dataIndex : 'Name'
-			}, {
-				text : 'Current Status',
-				dataIndex : 'ScheduleState',
-				renderer : function(value) {
-					return value;
+			modelNames : [ 'userstory' ],
+			toggleState : 'grid',
+			stateful : false,
+			plugins : [ {
+				ptype : 'rallygridboardactionsmenu',
+				menuItems : [ {
+					text : 'Export to csv',
+					handler : function() {
+						// console.log(Rally.ui.grid);
+						window.location = Rally.ui.grid.GridCsvExport.buildCsvExportUrl(this.down('rallygridboard').getGridOrBoard());
+					},
+					scope : this
+				}, {
+					text : 'Print',
+					handler : function() {
+						Ext.create('Rally.ui.grid.TreeGridPrintDialog', {
+							grid : this.down('rallygridboard').getGridOrBoard(),
+							treeGridPrinterConfig : {
+								largeHeaderText : 'Milestone Projectwise Assignment'
+							}
+						});
+					},
+					scope : this
+				} ],
+				buttonConfig : {
+					iconCls : 'icon-export'
 				}
-			}, {
-				text : 'Team',
-				dataIndex : 'Project'
-			}, {
-				text : 'Feature',
-				dataIndex : 'Feature'
 			} ],
-			store : gridStore,
-			enableEditing : false
+			gridConfig : {
+				columnCfgs : [ 'FormattedID', 'Name', 'ScheduleState', 'Project', 'Feature' ],
+				store : gridStore,
+				enableEditing : false,
+				enableRanking : false,
+				enableInlineAdd : false,
+				enableBulkEdit : false,
+				enableScheduleStateClickable : false,
+				showRowActionsColumn : false
+			},
+			height : this.getHeight()
 		});
 
 		Ext.getBody().unmask();
-	},
-
-	_onClickExport : function() {
-
-		var that = this;
-
-		if (/*@cc_on!@*/0) { //Exporting to Excel not supported in IE
-			Ext.Msg.alert('Error', 'Exporting to CSV is not supported in Internet Explorer. Please switch to a different browser and try again.');
-		} else if (this.down('#gridData')) {
-
-			Ext.getBody().mask('Exporting...');
-
-			setTimeout(function() {
-				var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-' + 'microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head>'
-						+ '<!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>'
-						+ '{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet>'
-						+ '</x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}' + '</table></body></html>';
-
-				var base64 = function(s) {
-					return window.btoa(unescape(encodeURIComponent(s)));
-				};
-				var format = function(s, c) {
-					return s.replace(/{(\w+)}/g, function(m, p) {
-						return c[p];
-					});
-				};
-				var table = that.getComponent('gridData');
-
-				var excel_data = '<tr>';
-				Ext.Array.each(table.getEl().dom.outerHTML.match(/<span .*?x-column-header-text.*?>.*?<\/span>/gm), function(column_header_span) {
-					excel_data += (column_header_span.replace(/span/g, 'td'));
-				});
-				excel_data += '</tr>';
-				Ext.Array.each(table.getEl().dom.outerHTML.match(/<tr .*?x-grid-row.*?<\/tr>/gm), function(line) {
-					excel_data += line.replace(/[^\011\012\015\040-\177]/g, '>>');
-				});
-
-				var fileName = that.selectedMilestoneObj[0].get('FormattedID') + " - " + that.selectedMilestoneObj[0].get('Name');
-
-				var ctx = {
-					worksheet : name || 'Worksheet',
-					table : excel_data
-				};
-				window.location.href = 'data:application/vnd.ms-excel;base64,' + base64(format(template, ctx));
-				Ext.getBody().unmask();
-			}, 500);
-		}
 	}
 
 });
